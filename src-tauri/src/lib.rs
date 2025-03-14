9c1496dd62d78c22783ed0aacee6db98fde9cf2c
chengliang.zhu <zclgudu1224@126.com>
2025-03-14 20:52:19 +0800

    增加日志打印

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,10 +9,20 @@ use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite};
 use std::env;
 use std::error::Error;
 use tauri::Manager;
+use tauri_plugin_log::{Target, TargetKind};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .targets([
+                    Target::new(TargetKind::Stdout),
+                    Target::new(TargetKind::LogDir { file_name: None }),
+                    Target::new(TargetKind::Webview),
+                ])
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             get_stock_infos,
